8a1f6850912b534fbd556eab2b5e3b8bf515665d
25HP6282 25HP6282 <25HP6282@administrators-iMac.local>
2026-02-04 19:00:36 +0800

    Update frontend with intro page component and package changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,6 @@
-import { Header } from "@/components/header";
-import { Hero } from "@/components/hero";
-import { TrustBar } from "@/components/trust-bar";
-import { DiscoverFundraisers } from "@/components/discover-fundraisers";
-import { StatsSection } from "@/components/stats-section";
-import { HowItWorks } from "@/components/how-it-works";
-import { Testimonials } from "@/components/testimonials";
-import { Footer } from "@/components/footer";
-import { Categories } from "@/components/categories";
+import IntroPage from "@/components/intro-page";
 
 
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-background">
-      <Header />
-      <main>
-        <Hero />
-        <TrustBar />
-        <DiscoverFundraisers />
-        <Categories />
-        <StatsSection />
-        <HowItWorks />
-        <Testimonials />
-      </main>
-      <Footer />
-    </div>
-  );
+  return <IntroPage />;
 }
